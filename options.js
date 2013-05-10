4b5f1830369ce127dfc98e76bfea6af7b6439738
TGeneral <tomcheung789@gmail.com>
2013-05-10 23:03:26 +0800

    redesigned themes and options page

--- options.js
+++ options.js
@@ -1,29 +1,32 @@
 
-function save_options() {
-  var select = document.getElementById("color");
-  var color = select.children[select.selectedIndex].value;
-  localStorage["scrollbar_color"] = color;
-  
-  var status = document.getElementById("save_status");
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+function save_options(v) {
+    localStorage["scrollbar_color"] = v;
+    restore_options();
+    var status = document.getElementById("save_status");
+    status.innerHTML = "Options Saved.";
+    setTimeout(function() {
+        status.innerHTML = "";
+    }, 750);
 }
 
 function restore_options() {
-  var favorite = localStorage["scrollbar_color"];
-  if (!favorite) {
-    return;
-  }
-  var select = document.getElementById("color");
-  for (var i = 0; i < select.children.length; i++) {
-    var child = select.children[i];
-    if (child.value == favorite) {
-      child.selected = "true";
-      break;
+    var favorite = localStorage["scrollbar_color"];
+    if (!favorite) return;
+    var light = document.getElementById("light");
+    var dark = document.getElementById("dark");
+    light.setAttribute("class", "item");
+    dark.setAttribute("class", "item");
+    switch(favorite){
+    case "Dark":
+        dark.setAttribute("class", "item select");
+        break;
+    case "Light":
+        light.setAttribute("class", "item select");
+        break;
+    default:
+        light.setAttribute("class", "item select");
     }
-  }
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#save_button').addEventListener('click', save_options);
+document.querySelector('#light').addEventListener('click',  function(){save_options("Light")});
+document.querySelector('#dark').addEventListener('click',  function(){save_options("Dark")});
--- scrollbar.js
+++ scrollbar.js
@@ -1,9 +1,9 @@
 function load_css(filename){
-	var fileref=document.createElement("link");
-	fileref.setAttribute("rel", "stylesheet");
-	fileref.setAttribute("type", "text/css");
-	fileref.setAttribute("href", chrome.extension.getURL(filename));
-	document.head.appendChild(fileref);
+	var t=document.createElement("link");
+	t.setAttribute("rel", "stylesheet");
+	t.setAttribute("type", "text/css");
+	t.setAttribute("href", chrome.extension.getURL(filename));
+	document.head.appendChild(t);
 }
 
 function load_scroll_bar(){
@@ -12,19 +12,22 @@ function load_scroll_bar(){
 	     	var favorite = response.tcolor;
 	     	switch(favorite){
 			case "Dark":
-				load_css("css/t-scroll-bars.css");
+				load_css("css/t-scroll-bars-dark.css");
 				break;
 			case "Light":
 				load_css("css/t-scroll-bars-light.css");
 				break;
 			default:
-				load_css("css/t-scroll-bars.css");
+				load_css("css/t-scroll-bars-light.css");
 			}
   	});
 }
 
+var exclude = /(mail\.google\.com|chrome\.google\.com\/webstore|drive\.google\.com|google\.com\/calendar|omgubuntu\.co\.uk)/i
+
 function filter_url(url){
-	load_scroll_bar();
+	if (!exclude.test(url))
+		load_scroll_bar();
 }
 
 filter_url(document.URL);
